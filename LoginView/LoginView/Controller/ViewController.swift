addf89149500c051c0c9b0ebe880c83efdec4148
sladuf <04fud27@gmail.com>
2022-09-20 03:34:35 +0900

    MVC 패턴 적용

---
+++ LoginView/LoginView/Controller/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  LoginView
+//
+//  Created by 김민령 on 2022/09/02.
+//
+
+import UIKit
+import SnapKit
+
+final class ViewController: UIViewController {
+    
+    private let loginView = LoginView()
+    
+    override func loadView() {
+        view = loginView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAddTarget()
+    }
+    
+    func setupAddTarget() {
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        loginView.passwdResetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func resetButtonTapped() {
+        let alert = UIAlertController(title: "비밀번호 변경", message: "비밀번호를 바꾸시겠습니까?", preferredStyle: .alert)
+        
+        let success = UIAlertAction(title: "확인", style: .default) { action in
+            print("확인 : \(action)")
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { cancel in
+            print("취소 : \(cancel)")
+        }
+        
+        alert.addAction(success)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @objc func loginButtonTapped() {
+        
+    }
+    
+}
+
---
+++ LoginView/LoginView/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  LoginView
+//
+//  Created by 김민령 on 2022/09/17.
+//
+
+import Foundation
