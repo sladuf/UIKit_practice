c7612d35763b12edce4fb54d743b9144f286f416
sladuf <04fud27@gmail.com>
2023-01-30 05:28:37 +0900

    BMI calculator with storyboard

---
+++ BMI/BMI/BMIResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BMIResultViewController.swift
+//  BMI
+//
+//  Created by 김민령 on 2023/01/30.
+//
+
+import UIKit
+
+class BMIResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
