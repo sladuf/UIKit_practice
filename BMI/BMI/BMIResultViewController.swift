f4efc37d7834cd3f24a9f51f47e05004b0a04c61
sladuf <04fud27@gmail.com>
2023-01-30 05:31:40 +0900

    BMI calculator with storyboard

--- BMI/BMI/BMIResultViewController.swift
+++ BMI/BMI/BMIResultViewController.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class BMIResultViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
+    
+    var result : Float?
+    var bmi : BMI?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = result?.description
+        BMIresult(result!)
+        initAttribute()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func tapBackButton(_ sender: Any) {
+        dismiss(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initAttribute(){
+        
+        switch bmi {
+        case .under:
+            resultLabel.backgroundColor = .green
+            subLabel.text = "저체중"
+        case .standard:
+            resultLabel.backgroundColor = .yellow
+            subLabel.text = "표준"
+        case .over:
+            resultLabel.backgroundColor = .systemPink
+            subLabel.text = "과체중"
+        case .moderate:
+            resultLabel.backgroundColor = .cyan
+            subLabel.text = "중도 비만"
+        case .severe:
+            resultLabel.backgroundColor = .red
+            subLabel.text = "고도 비만"
+        case .none:
+            return
+        }
     }
-    */
-
+    
+    
+    func BMIresult(_ num : Float) {
+        
+        switch num {
+        case ...18.5 :
+            bmi = .under
+        case ...22.9 :
+            bmi = .standard
+        case ...24.9 :
+            bmi = .over
+        case ...29.9 :
+            bmi = .moderate
+        default :
+            bmi = .severe
+        }
+        
+    }
 }
+
+enum BMI {
+    case under
+    case standard
+    case over
+    case moderate //중도비만
+    case severe //고도비만
+}
+
--- BMI/BMI/ViewController.swift
+++ BMI/BMI/ViewController.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightTextField.delegate = self
+        weightTextField.delegate = self
+        
+        initAttribute()
+    }
+    
+    func initAttribute(){
+        heightTextField.placeholder = "cm단위로 입력"
+        weightTextField.placeholder = "kg단위로 입력"
     }
 
+    @IBAction func tapResultButton(_ sender: Any) {
+        guard let result = calculateBMI(height: heightTextField.text!, weight: weightTextField.text!) else {
+            return
+        }
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "BMI") as! BMIResultViewController
+        vc.modalPresentationStyle = .fullScreen
+        
+        vc.result = result
+        present(vc, animated: true)
+        
+    }
+    
+    func calculateBMI(height: String, weight: String) -> Float?{
+        guard let h = Float(height), let w = Float(weight) else { return nil }
+        
+        var bmi = w / (h*h) * 10000
+        bmi = round(bmi*10) / 10
+        return bmi
 
+    }
+    
+}
+
+extension ViewController : UITextFieldDelegate {
+    
+    //숫자만 입력 가능하게 조절
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if Int(string) != nil || string == "" {
+            return true
+        }
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //두개 다 채웠으면 -> 키보드 내려
+        //키만 채웠으면 -> 몸무게로 가
+        //아니면 false
+        if heightTextField.text != "", weightTextField.text != "" {
+            self.view.endEditing(true)
+        } else if heightTextField.text != "" {
+            weightTextField.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
